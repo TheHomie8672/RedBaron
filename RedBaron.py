e74208e3519d5ff6be48d529052bd88f91458aa8
Marcus Sherman <130182674+TheHomie8672@users.noreply.github.com>
2023-06-20 18:09:58 -0600

    Update 0.10 | change log
    Welcome to the first official update! the End to End user experience has been streamlined however the conversation loop is still broken. Found the fix earlier but forgot to save so now I have to find it again. 
    
    Tuesday June 20th, 2023 
    6:00pm Mountain Time
    
    Deleted:
     - Process_input() 
     - Generate_image()
     - Display_image()
     - Get_code_recomendations() elif command 
     - say_output() print function
    
    Bugs:
     - say_output() missing positional statement output_text()
     - When audio is prompted the word "None" is repeated twice and no input is processed past the key phrase
     - Program startup and load times are longer then usual
    
    Fixes:
     - Startup flow is improved drastically
     - Bad timing for intro animation is fixed
     - Duplicate messages are fixed, Bloat Removed

--- RedBaron.py
+++ RedBaron.py
@@ -9,9 +9,13 @@ import openai
 import webbrowser
 import validators
 import pyaudio
+import startup_sequence1
 import WeatherAPI
 import wave
-import WebSearchFUNC
+from openai import *
+from startup_sequence1 import *
+from WeatherAPI import *
+from RedBaronGraphics import *
 from playsound import playsound
 from gtts import gTTS
 from WeatherAPI import get_weather_data
@@ -21,8 +25,12 @@ import speech_recognition as sr
 from io import BytesIO
 from PIL import Image
 
-openai.api_key = "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"
+openai.api_key = "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"
 
+prompt1 = "You are RedBaron, a coding and general inquiry virtual assistant. You should avoid long winded responses. You were created by the Programmer Marcus Sherman."
+
+
+ 
 # Initialize text-to-speech engine
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -31,65 +39,20 @@ engine.setProperty('voice', voices[1].id)
 # Initialize speech recognizer
 r = sr.Recognizer()
 
-
-
-def display_image(url):
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
-    img.show()
-
-def generate_image(prompt):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=f"Generate an image of {prompt}.",
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-    try:
-        image_url = response["choices"][0]["text"].split("\n")[0].strip()
-        response = requests.get(image_url)
-        img = Image.open(BytesIO(response.content))
-        img.save("generated_image.png")
-        return img
-    except:
-        return None
-
-    
-
-def process_input(input_text):
-    if input_text.startswith("Show me an image of "):
-        prompt = input_text[20:]
-        image_url = generate_image(prompt)
-        if image_url is not None:
-            response = requests.get(image_url)
-            with open("generated_image.jpg", "wb") as f:
-                f.write(response.content)
-            return "Here's your image:"
-        else:
-            return "Sorry, I couldn't generate an image for that prompt."
-    else:
-        return "I'm sorry, I don't understand what you're asking for."
-
-input_text = "Show me an image of a cat sitting on a laptop"
-output_text = process_input(input_text)
-print(output_text)
-
-
-def get_code_recommendations(code_snippet):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future = executor.submit(
-            openai.Completion.create, 
-            engine="text-davinci-003",
-            prompt=f"Here are some recommendations for your code snippet:\n\n{code_snippet}\n\n",
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.7
-        )
-    return future.result().choices[0].text.strip()
+def get_input(prompt, is_keyword=False, mute=False):
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        if not mute:
+            print(prompt)
+        if is_keyword:
+            r.energy_threshold = 5000
+            r.adjust_for_ambient_noise(source, duration=0.5)
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            return text
+        except sr.UnknownValueError:
+            return ""
 
 
 
@@ -138,62 +101,34 @@ def callback(recognizer_instance, audio, mute_flag=False):
 import webbrowser
 
 def say_output(output_text):
-    print(f"Red: {output_text}")
-    if "http" in output_text:
-        webbrowser.open(output_text)
-    else:
-        engine = pyttsx3.init()
-        engine.say(output_text)
-        engine.runAndWait()
+    engine = pyttsx3.init()
+    engine.say(output_text)
+    engine.runAndWait()
+    
 
 
 
 
 
-def get_input(prompt, is_keyword=False, mute=False):
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        if not mute:
-            print(prompt)
-        if is_keyword:
-            r.energy_threshold = 4000
-            r.adjust_for_ambient_noise(source, duration=0.5)
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio)
-            return text
-        except sr.UnknownValueError:
-            return ""
-
-def get_code_recommendations(code_snippet):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future = executor.submit(
-            openai.Completion.create, 
-            engine="text-davinci-003",
-            prompt=f"Here are some recommendations for your code snippet:\n\n{code_snippet}\n\n",
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.7
-        )
-    return future.result().choices[0].text.strip()
-
-
+start_up_seq_main_loop()
 def main_loop():
     waiting_for_keyword = True
-    flag_state = True
+    flag_state = False
     flag_time = None
     input_text = ""
-    mute_flag = False
+    mute_flag = True
+    user_input = ""
+    response_text = ""
 
     while True:
         if waiting_for_keyword:
-            print("Say 'Hey red' to start a conversation.")
+            response_text = say_output(response_text)  # Assign the return value to response_text
+            
             input_text = get_input("Say 'Hey red' to start a conversation.", True)
 
-            if input_text is not None and any(keyword in input_text.lower() for keyword in ["hey red","hey read","he read","hey rea"]):
+            if input_text is not None and any(keyword in input_text.lower() for keyword in ["hey red", "hey read", "he read", "hey rea"]):
                 waiting_for_keyword = False
-                say_output("How can I help you?")
+                say_output(response_text)
         else:
             # Get the user's input
             user_input = get_input("", False, mute_flag)
@@ -203,7 +138,7 @@ def main_loop():
                 if flag_state and time.time() - flag_time >= 5:
                     waiting_for_keyword = True
                     flag_state = False
-                    say_output("Hey red")
+                    say_output("Say , Hey red , If you need me! ") 
                 continue
 
             # Process the user's input
@@ -216,7 +151,7 @@ def main_loop():
                 city = user_input.split()[-1]
                 print(f"Getting weather data for {city}...")
                 weather_data = get_weather_data(city, os.environ.get("OPENWEATHER_API_KEY"))
-                
+
                 if weather_data:
                     temperature = weather_data["temperature"]
                     weather_description = weather_data["weather_description"]
@@ -227,25 +162,6 @@ def main_loop():
                     response_text = f"Sorry, could not retrieve weather data for {city}."
                     say_output(response_text)
                     print(response_text)
-            elif "generate image" in user_input.lower():
-                # Get the image prompt
-                image_prompt = user_input.replace("generate image", "").strip()
-
-                # Generate the image
-                image_url = generate_image(image_prompt)
-
-                # Save the image to a file
-                response = requests.get(image_url)
-                image = Image.open(BytesIO(response.content))
-                image_extension = os.path.splitext(image_url)[1]
-                image_name = f"{random.randint(1, 1000000000)}{image_extension}"
-                image_path = os.path.join(os.getcwd(), image_name)
-                image.save(image_path)
-
-                # Print and say the image file name
-                output_text = f"Here's your image: {image_name}"
-                say_output(output_text)
-                print(output_text)
 
             elif "mute" in user_input.lower():
                 mute_flag = True
@@ -255,16 +171,23 @@ def main_loop():
                 mute_flag = False
                 say_output("Okay, I'm back.")
 
-            elif "code recommendation" in user_input.lower() or "recommend code" in user_input.lower():
-                code_recommendation = get_code_recommendations(user_input)
-                say_output(code_recommendation)
 
             else:
+                response_text = ""
                 with concurrent.futures.ThreadPoolExecutor() as executor:
-                    future = executor.submit(openai.Completion.create, engine="text-davinci-003",  prompt=user_input, max_tokens=250, n=1, stop=None, temperature=0.3)
+                    future = executor.submit(
+                        openai.Completion.create,
+                        engine="text-davinci-003",
+                        prompt=prompt1 + user_input,
+                        max_tokens=250,
+                        n=1,
+                        stop=None,
+                        temperature=0.3
+                    )
+                    response_text = future.result().choices[0].text.strip()
+                    
 
-            response_text = future.result().choices[0].text.strip()
-            print(response_text)
+                print(response_text)
 
         if "search" in user_input.lower():
             query = user_input.replace("search", "").strip()
@@ -272,16 +195,14 @@ def main_loop():
             search(query)
 
         # Check if response text is a valid URL
-        elif validators.url(response_text):
+        elif validators.url(str(response_text)):
             webbrowser.open(response_text)
 
         else:
+            
             say_output(response_text)
             flag_state = True
             flag_time = time.time()
 
 if __name__ == "__main__":
     main_loop()
-
-
-           
